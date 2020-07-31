ecf1dfebfb638360b561b8c8c2907b0814aea6ae
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-07-31 17:44:38 +0700

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: {
+      files: "scss/**/*.scss",
+      tasks: ["sass"],
+    },
+    sass: {
+      dev: {
+        files: {
+          "css/main.css": "scss/main.scss",
+        },
+      },
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ["css/*.css", "*.html"],
+        },
+        options: {
+          watchTask: true,
+          server: ".",
+        },
+      },
+    },
+  });
+
+  //   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-browser-sync");
+
+  grunt.registerTask("default", ["browserSync", "watch", "sass"]);
+};
