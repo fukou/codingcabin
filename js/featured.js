f3c9da32bf899b33f6af6f35574d03d8ec617250
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-01 10:51:49 +0700

    Finished the featured section and styling

--- js/featured.js
+++ js/featured.js
@@ -11,7 +11,7 @@ const url =
   ".tumblr.com/posts/photo?api_key=" +
   apiKey;
 
-console.log(url);
+// console.log(url);
 const container = document.querySelector(".wrapper__featured--inner__content");
 
 fetch(url)
@@ -22,13 +22,38 @@ fetch(url)
 
     for (let i = 1; i <= 2; i++) {
       let posts = read.posts[i];
-      let { caption, photos } = posts;
+      let {
+        date,
+        note_count,
+        post_url,
+        photos: [
+          {
+            original_size: { url: photo_url },
+          },
+        ],
+        trail: [
+          {
+            blog: { name: username },
+          },
+        ],
+      } = posts;
 
       const article = document.createElement("div");
       article.className = "grid-content";
-      article.innerHTML = '<div class="body__text">' + caption + "</div>";
+      article.innerHTML = `
+      <div class="grid-content">
+        <img src="${photo_url}">
+        <div class="body__text">
+            <div class="body__text--date">${date}</div>
+            <h2>Created by ${username}</h2>
+            <div class="body__text--note">${note_count} notes</div>
+            <a role="button" tabindex="0" class="btn-read" href="${post_url}">Keep reading</div>
+        </div>
+      </div>
+      `;
 
       container.appendChild(article);
+      console.log(photo_url);
 
       const tumblr_blog = document.querySelectorAll(".tumblr_blog");
       for (let j = 0; j < tumblr_blog.length; j++) {
