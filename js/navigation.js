994b0c44b280e1c92f77ce652e10be1602a740de
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-02 08:40:01 +0700

    Modified the navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,10 +2,14 @@ const buttonOpen = document.querySelector(".nav__inner--mobile button");
 const buttonClose = document.querySelector(".close-btn");
 const wrapper = document.querySelector(".wrapper__mobile");
 
-buttonOpen.addEventListener("click", () => {
+buttonOpen.addEventListener("click", (e) => {
   wrapper.classList.add("is-shown");
+
+  e.preventDefault();
 });
 
-buttonClose.addEventListener("click", () => {
+buttonClose.addEventListener("click", (e) => {
   wrapper.classList.remove("is-shown");
+
+  e.preventDefault();
 });
