5c7a52c87af6f1a4eb69242da801993467ef065a
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-02 08:36:11 +0700

    Added a navigation menu on mobile screen

--- js/navigation.js
+++ js/navigation.js
@@ -1,2 +1,11 @@
-const button = document.querySelector(".nav__inner--mobile button");
-const wrapper;
+const buttonOpen = document.querySelector(".nav__inner--mobile button");
+const buttonClose = document.querySelector(".close-btn");
+const wrapper = document.querySelector(".wrapper__mobile");
+
+buttonOpen.addEventListener("click", () => {
+  wrapper.classList.add("is-shown");
+});
+
+buttonClose.addEventListener("click", () => {
+  wrapper.classList.remove("is-shown");
+});
