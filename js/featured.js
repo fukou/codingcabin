41b8585864a0cbcc254f3d0d60b289468cae34c5
anna <56127468+anna-qiu@users.noreply.github.com>
2020-08-01 19:28:33 -0700

    initialize the tiny slider after the promise is completed

--- js/featured.js
+++ js/featured.js
@@ -37,16 +37,16 @@ fetch(url)
         date,
         note_count,
         post_url,
-        photos: [
-          {
-            original_size: { url: photo_url },
+        photos: [{
+          original_size: {
+            url: photo_url
           },
-        ],
-        trail: [
-          {
-            blog: { name: username },
+        }, ],
+        trail: [{
+          blog: {
+            name: username
           },
-        ],
+        }, ],
       } = posts;
 
       const article = document.createElement("div");
@@ -79,54 +79,66 @@ fetch(url)
   })
   .catch((err) => console.log(err));
 
-// const blog__wrapper = document.querySelector(
-//   ".wrapper__blog--inner__slideshow"
-// );
+const blog__wrapper = document.querySelector(
+  ".wrapper__blog--inner__slideshow"
+);
 
-// fetch(blog)
-//   .then((res) => res.json())
-//   .then((data) => {
-//     let read = data.response;
-//     console.log(read);
+fetch(blog)
+  .then((res) => res.json())
+  .then((data) => {
+    let read = data.response;
+    console.log(read);
 
-//     for (let i = 1; i <= 2; i++) {
-//       let posts = read.posts[i];
-//       let {
-//         date,
-//         note_count,
-//         caption,
-//         post_url,
-//         photos: [
-//           {
-//             original_size: { url: photo_url },
-//           },
-//         ],
-//       } = posts;
+    for (let i = 1; i <= 2; i++) {
+      let posts = read.posts[i];
+      let {
+        date,
+        note_count,
+        caption,
+        post_url,
+        photos: [{
+          original_size: {
+            url: photo_url
+          },
+        }, ],
+      } = posts;
 
-//       //
-//       const slide = document.createElement("div");
-//       slide.className = "grid-slideshow";
-//       slide.innerHTML = `
-//       <div class="grid-slideshow__inner">
-//         <div class="grid-slideshow__img">
-//           <img src="${photo_url}" alt="" />
-//         </div>
+      //
+      const slide = document.createElement("div");
+      slide.className = "grid-slideshow";
+      slide.innerHTML = `
+      <div class="grid-slideshow__inner">
+        <div class="grid-slideshow__img">
+          <img src="${photo_url}" alt="" />
+        </div>
 
-//         <div class="grid-slideshow__desc">
-//           ${caption}
-//         </div>
-//       </div>
-//       `;
+        <div class="grid-slideshow__desc">
+          ${caption}
+        </div>
+      </div>
+      `;
 
-//       blog__wrapper.appendChild(slide);
-//       console.log(photo_url);
+      blog__wrapper.appendChild(slide);
+      console.log(photo_url);
 
-//       const tumblr_blog = document.querySelectorAll(".tumblr_blog");
-//       for (let j = 0; j < tumblr_blog.length; j++) {
-//         tumblr_blog[j].parentNode.parentNode.removeChild(
-//           tumblr_blog[j].parentNode
-//         );
-//       }
-//     } // end of response
-//   })
-//   .catch((err) => console.log(err));
+      const tumblr_blog = document.querySelectorAll(".tumblr_blog");
+      for (let j = 0; j < tumblr_blog.length; j++) {
+        tumblr_blog[j].parentNode.parentNode.removeChild(
+          tumblr_blog[j].parentNode
+        );
+      }
+    } // end of response
+
+    const slider = tns({
+      container: ".wrapper__blog--inner__slideshow",
+      items: 1,
+      slideBy: "page",
+      loop: false,
+      autoplay: true,
+      controlsText: [
+        '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>',
+        '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h13M12 5l7 7-7 7"/></svg>',
+      ],
+    });
+  })
+  .catch((err) => console.log(err));
