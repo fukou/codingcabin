da428b44f52eb0c7bc620cecc62424000fe499ee
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-06 18:05:57 +0700

    Added a blog section

--- js/navigation.js
+++ js/navigation.js
@@ -2,14 +2,14 @@ const buttonOpen = document.querySelector(".nav__inner--mobile button");
 const buttonClose = document.querySelector(".close-btn");
 const wrapper = document.querySelector(".wrapper__mobile");
 
-buttonOpen.addEventListener("click", function () {
+buttonOpen.addEventListener("click", function (e) {
   wrapper.classList.add("is-shown");
   this.setAttribute("aria-expanded", "true");
 
   e.preventDefault();
 });
 
-buttonClose.addEventListener("click", function () {
+buttonClose.addEventListener("click", function (e) {
   wrapper.classList.remove("is-shown");
   buttonOpen.setAttribute("aria-expanded", "false");
   e.preventDefault();
