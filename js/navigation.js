a7a16cd6037891e665b27db67f26414aca1ad44e
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-14 09:12:37 +0700

    Add a functionality on the keyboard users

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,4 @@
+//  mobile navigation
 const buttonOpen = document.querySelector(".nav__inner--mobile button");
 const buttonClose = document.querySelector(".close-btn");
 const wrapper = document.querySelector(".wrapper__mobile");
@@ -13,4 +14,11 @@ buttonClose.addEventListener("click", function (e) {
   wrapper.classList.remove("is-shown");
   buttonOpen.setAttribute("aria-expanded", "false");
   e.preventDefault();
+});
+
+// disable the links to be highlighted by keyboard users
+const linkSlideshow = document.querySelectorAll(".grid-slideshow__desc a");
+linkSlideshow.forEach(function (item, idx) {
+  item.setAttribute("tabindex", "-1");
+  item.setAttribute("aria-hidden", "true");
 });
