3193ea596f653d688c51be2e9386a46760091c23
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-15 21:23:39 +0700

    Added a dark mode

--- js/navigation.js
+++ js/navigation.js
@@ -18,10 +18,33 @@ buttonClose.addEventListener("click", function (e) {
   e.preventDefault();
 });
 
+// active state link
 const currentLink = location.pathname;
 const navLink = document.querySelectorAll(".nav__inner--links ul li a");
 navLink.forEach(function (item, idx) {
   if (item.getAttribute("href") === currentLink) {
     item.parentElement.classList.add("active");
   }
+});
+
+// dark mode
+const btn_dark = document.querySelector(".btn-dark a");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn_dark.addEventListener("click", function (e) {
+  e.preventDefault();
+  this.classList.toggle("dark-actived");
+
+  document.body.classList.toggle("dark-theme");
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+
+  localStorage.setItem("theme", theme);
 });
