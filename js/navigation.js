e6d35e671bede3bf66b9f2d9d192430a787b122f
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-08-16 13:28:20 +0700

    Tweaked the colors on dark mode

--- js/navigation.js
+++ js/navigation.js
@@ -29,10 +29,13 @@ navLink.forEach(function (item, idx) {
 
 // dark mode
 const btn_dark = document.querySelector(".btn-dark a");
-
 const currentTheme = localStorage.getItem("theme");
+
 if (currentTheme == "dark") {
   document.body.classList.add("dark-theme");
+  btn_dark.innerHTML = `<svg viewBox="0 0 15 15" fill="hsl(60, 100%, 68%)" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M1.66 11.362A6.5 6.5 0 007.693.502a7 7 0 11-6.031 10.86z" stroke="currentColor" stroke-linejoin="round"></path></svg>`;
+} else {
+  btn_dark.innerHTML = `<svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M7.5 1.5v-1m0 13.99v-.998m6-5.997h1m-13 0h-1m2-4.996l-1-1m12 0l-1 1m-10 9.993l-1 1m12 0l-1-1m-2-4.997a2.999 2.999 0 01-3 2.998 2.999 2.999 0 113-2.998z" stroke="currentColor" stroke-linecap="square"></path></svg>`;
 }
 
 btn_dark.addEventListener("click", function (e) {
@@ -40,12 +43,11 @@ btn_dark.addEventListener("click", function (e) {
   this.classList.toggle("dark-actived");
 
   document.body.classList.toggle("dark-theme");
-  console.log(this.innerHTML);
 
   let theme = "light";
+
   if (document.body.classList.contains("dark-theme")) {
     theme = "dark";
-
     this.innerHTML = `<svg viewBox="0 0 15 15" fill="hsl(60, 100%, 68%)" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M1.66 11.362A6.5 6.5 0 007.693.502a7 7 0 11-6.031 10.86z" stroke="currentColor" stroke-linejoin="round"></path></svg>`;
   } else {
     this.innerHTML = `<svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M7.5 1.5v-1m0 13.99v-.998m6-5.997h1m-13 0h-1m2-4.996l-1-1m12 0l-1 1m-10 9.993l-1 1m12 0l-1-1m-2-4.997a2.999 2.999 0 01-3 2.998 2.999 2.999 0 113-2.998z" stroke="currentColor" stroke-linecap="square"></path></svg>`;
